217fc79d956a4cef2dde1f9098bdf9a96d750132
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-07-21 11:45:04 +0200

    - Inserito Penis e prima prova di Waypoint nel circuito

---
+++ Assets/Scripts/EditorPathScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorPathScript : MonoBehaviour
+{
+    public Color rayColor = Color.white;
+    public List<Transform> pathList = new List<Transform>();
+    Transform[] pathArray;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        pathArray = GetComponentsInChildren<Transform>();
+        pathList.Clear();
+
+        foreach (var path in pathArray)
+        {
+            if (path != this.transform)
+            {
+                pathList.Add(path);
+            }
+        }
+
+        for (int i = 0; i < pathList.Count; i++)
+        {
+            Vector3 position = pathList[i].position;
+            if (i > 0)
+            {
+                Vector3 previous = pathList[i - 1].position;
+                Gizmos.DrawLine(previous, position);
+                Gizmos.DrawWireSphere(position, .3f);
+            }
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/MoveOnPathScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnPathScript : MonoBehaviour
+{
+    public EditorPathScript pathToFollow;
+
+    public int currentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 1.0f;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+
+    Vector3 last_position, current_position;
+
+    private void Start()
+    {
+        //pathToFollow = GameObject.Find(pathName).GetComponent<EditorPathScript>();
+        last_position = transform.position;
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(pathToFollow.pathList[currentWayPointID].position, this.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, pathToFollow.pathList[currentWayPointID].position, Time.deltaTime * speed);
+
+        Quaternion rotation = Quaternion.LookRotation(pathToFollow.pathList[currentWayPointID].position - this.transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+
+        if (distance <= reachDistance)
+        {
+            currentWayPointID++;
+        }
+
+        if (currentWayPointID >= pathToFollow.pathList.Count)
+        {
+            currentWayPointID = 0;
+        }
+
+    }
+
+}
