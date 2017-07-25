a435463abed51e7579d486d09b968929fd7e9385
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-07-25 16:44:26 +0200

    - Inserito animazioni del player principale e spostamento lungo le varie lane di gioco. inoltre ora sembra passare le clip audio in modo automatico

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -5,13 +5,14 @@ using System;
 
 public class MovePlayer : MonoBehaviour
 {
-    public bool isMovingLane = false;
+    private bool isMovingLane = false;
     public int numLane = 0;
     public float speed;
     private float x0, x1, x2, xless1, xless2;
 
     private SoundManager refSM;
     private Rigidbody rb;
+    private Animator anim;
 
     public KeyCode moveR, moveL;
 
@@ -20,6 +21,7 @@ public class MovePlayer : MonoBehaviour
     private void Start()
     {
         refSM = FindObjectOfType<SoundManager>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         x0 = refSM.lane_0.localPosition.x;
         x1 = refSM.lane_1.localPosition.x;
@@ -35,42 +37,59 @@ public class MovePlayer : MonoBehaviour
         if (Input.GetKeyDown(moveL) && !isMovingLane && numLane > -2)
         {
             numLane--;
+            anim.SetBool("isTurnLeft", true);
             delCurrentLane(numLane);
             ChangeLane(numLane);
+            StartCoroutine(SetFalseBool("isTurnLeft"));
         }
 
         if (Input.GetKeyDown(moveR) && !isMovingLane && numLane < 2)
         {
+            anim.SetBool("isTurnRight", true);
             numLane++;
             delCurrentLane(numLane);
             ChangeLane(numLane);
+            StartCoroutine(SetFalseBool("isTurnRight"));
         }
     }
 
     private void ChangeLane(int _numLane)
     {
-        isMovingLane = true;
-
         switch (_numLane)
         {
             case 0:
-                this.transform.position = new Vector3(x0, this.transform.position.y, this.transform.position.z);
+                StartCoroutine(SwitchLaneCO(x0));
                 break;
             case 1:
-                this.transform.position = new Vector3(x1, this.transform.position.y, this.transform.position.z);
+                StartCoroutine(SwitchLaneCO(x1));
                 break;
             case 2:
-                this.transform.position = new Vector3(x2, this.transform.position.y, this.transform.position.z);
+                StartCoroutine(SwitchLaneCO(x2));
                 break;
             case -1:
-                this.transform.position = new Vector3(xless1, this.transform.position.y, this.transform.position.z);
+                StartCoroutine(SwitchLaneCO(xless1));
                 break;
             case -2:
-                this.transform.position = new Vector3(xless2, this.transform.position.y, this.transform.position.z);
+                StartCoroutine(SwitchLaneCO(xless2));
                 break;
         }
+    }
 
-
+    private IEnumerator SwitchLaneCO(float _x)
+    {
+        isMovingLane = true;
+        while (this.transform.position.x != _x)
+        {
+            this.transform.position = Vector3.MoveTowards(transform.position, new Vector3(_x, this.transform.position.y, this.transform.position.z), Time.deltaTime * 7);
+            yield return null;
+        }
         isMovingLane = false;
+    }
+
+    private IEnumerator SetFalseBool(string _bool)
+    {
+        yield return new WaitForSeconds(.1f);
+        anim.SetBool(_bool, false);
+
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,11 +6,23 @@ public class SoundManager : MonoBehaviour
 {
     private MovePlayer refMP;
     public Transform lane_0, lane_1, lane_2, lane_less_1, lane_less_2;
+    public AudioClip[] audioClipArray;
+    [SerializeField]
+    private Transform[] laneArray = new Transform[4];
 
-    private void Start()
+    public int currIndexAudioClipArray;
+
+    private void Awake()
     {
         refMP = FindObjectOfType<MovePlayer>();
         refMP.delCurrentLane = PlaySingleTrack;
+        laneArray[0] = lane_1;
+        laneArray[1] = lane_2;
+        laneArray[2] = lane_less_1;
+        laneArray[3] = lane_less_2;
+
+        SetAudioClip();
+        StartCoroutine(ChangeAudioClipCO());
     }
 
     private void PlaySingleTrack(int _numLane)
@@ -20,46 +32,47 @@ public class SoundManager : MonoBehaviour
             case 0:
                 StartCoroutine(IncreaseVolumeCO(lane_0));
 
-                StartCoroutine(DecreaseVolumeCO(lane_1));
-                StartCoroutine(DecreaseVolumeCO(lane_2));
-                StartCoroutine(DecreaseVolumeCO(lane_less_1));
-                StartCoroutine(DecreaseVolumeCO(lane_less_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_2));
                 break;
             case 1:
                 StartCoroutine(IncreaseVolumeCO(lane_1));
 
-                StartCoroutine(DecreaseVolumeCO(lane_0));
-                StartCoroutine(DecreaseVolumeCO(lane_2));
-                StartCoroutine(DecreaseVolumeCO(lane_less_1));
-                StartCoroutine(DecreaseVolumeCO(lane_less_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_0));
+                //StartCoroutine(DecreaseVolumeCO(lane_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_2));
                 break;
             case 2:
                 StartCoroutine(IncreaseVolumeCO(lane_2));
 
-                StartCoroutine(DecreaseVolumeCO(lane_1));
-                StartCoroutine(DecreaseVolumeCO(lane_0));
-                StartCoroutine(DecreaseVolumeCO(lane_less_1));
-                StartCoroutine(DecreaseVolumeCO(lane_less_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_0));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_2));
                 break;
             case -1:
                 StartCoroutine(IncreaseVolumeCO(lane_less_1));
 
-                StartCoroutine(DecreaseVolumeCO(lane_1));
-                StartCoroutine(DecreaseVolumeCO(lane_2));
-                StartCoroutine(DecreaseVolumeCO(lane_0));
-                StartCoroutine(DecreaseVolumeCO(lane_less_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_0));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_2));
 
                 break;
             case -2:
                 StartCoroutine(IncreaseVolumeCO(lane_less_2));
 
-                StartCoroutine(DecreaseVolumeCO(lane_1));
-                StartCoroutine(DecreaseVolumeCO(lane_2));
-                StartCoroutine(DecreaseVolumeCO(lane_less_1));
-                StartCoroutine(DecreaseVolumeCO(lane_0));
+                //StartCoroutine(DecreaseVolumeCO(lane_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_2));
+                //StartCoroutine(DecreaseVolumeCO(lane_less_1));
+                //StartCoroutine(DecreaseVolumeCO(lane_0));
                 break;
         }
     }
+
     private IEnumerator IncreaseVolumeCO(Transform _lane)
     {
         _lane.GetComponent<AudioSource>().volume = 1;
@@ -68,9 +81,9 @@ public class SoundManager : MonoBehaviour
         //{
         //    yield return null;
         //    _lane.GetComponent<AudioSource>().volume += .1f;
-
         //}
     }
+
     private IEnumerator DecreaseVolumeCO(Transform _lane)
     {
         while (_lane.GetComponent<AudioSource>().volume != 0)
@@ -79,5 +92,26 @@ public class SoundManager : MonoBehaviour
             _lane.GetComponent<AudioSource>().volume -= .05f;
 
         }
+    }
+
+    private void SetAudioClip()
+    {
+        foreach (var lane in laneArray)
+        {
+            for (int i = currIndexAudioClipArray; i < currIndexAudioClipArray + 1; i++)
+            {
+                lane.GetComponent<AudioSource>().clip = audioClipArray[i];
+                lane.GetComponent<AudioSource>().Play();
+            }
+            currIndexAudioClipArray++;
+        }
+        Debug.Log(currIndexAudioClipArray);
+    }
+
+
+    private IEnumerator ChangeAudioClipCO()
+    {
+        yield return new WaitForSeconds(14f);
+        SetAudioClip();
     }
 }
